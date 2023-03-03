df4bc0775f9830b15da76c1ef8b58deb30596e35
Thullyo <thullyodamasceno.3@gmail.com>
2023-03-03 17:02:48 -0300

    add task funtionality done

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,84 @@ import { Task } from './components/Task';
 import './global.css';
 import styles from './App.module.css'
 
-import { tasksList } from './components/NewTask'
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+interface taskStructure {
+  id: number;
+  taskTitle: string;
+  isComplete: boolean;
+}
+
+const initialTasks: taskStructure[] = [
+  {
+    id: 1,
+    taskTitle: "Study the React fundamentals",
+    isComplete: true,
+  },
+  {
+    id: 2,
+    taskTitle: "Build a social media feed with React and TypeScript",
+    isComplete: true,
+  },
+  {
+    id: 3,
+    taskTitle: "Build a to-do list with React and TypeScript",
+    isComplete: false,
+  },
+  {
+    id: 4,
+    taskTitle: "Study how to create a SPA with React",
+    isComplete: false,
+  }
+];
 
 export function App() {
-  const hasTask: boolean = tasksList.length !== 0;
+  const [tasks, setTasks] = useState<taskStructure[]>(initialTasks)
+
+  const [newTaskTitle, setNewTaskTitle] = useState<string>('')
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+
+    // The reduce method will search for the biggest task id
+    const biggestId = tasks.reduce((acc, task) => {
+      return task.id > acc ? task.id : acc
+    }, 0)
+    
+    const newTaskId = biggestId + 1;
+
+    const newTask = {
+      id: newTaskId,
+      taskTitle: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks([...tasks, newTask])
+    setNewTaskTitle('');
+  }
+  
+  function handleNewTaskChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    setNewTaskTitle(event.target.value)
+  }
+  
+  const hasTask = tasks.length !== 0;
 
   return (
     <div>
       <Header />
 
       <main className={styles.main}>
-        <NewTask />
+        <NewTask 
+          onNewTaskChange={handleNewTaskChange}
+          newTaskTitle={newTaskTitle}
+          onCreateNewTask={handleCreateNewTask}
+        />
         
         <section>
           <Infos />     
 
           {hasTask ? 
-            tasksList.map(task => {
+            tasks.map(task => {
               return (
                 <Task 
                   key={task.id}
--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,69 +1,28 @@
 import { PlusCircle } from 'phosphor-react';
-import { ChangeEvent, FormEvent, useState } from 'react';
 import styles from './NewTask.module.css'
 
-export interface Tasks {
-  id: number;
-  taskTitle: string;
-  isComplete: boolean;
+// Is this interface right?
+interface NewTaskProps {
+  onCreateNewTask: Function;
+  newTaskTitle: string;
+  onNewTaskChange: Function;
 }
 
-export const tasks: Tasks[] = [
-  {
-    id: 1,
-    taskTitle: "Study the React fundamentals",
-    isComplete: true,
-  },
-  {
-    id: 2,
-    taskTitle: "Build a social media feed with React and TypeScript",
-    isComplete: true,
-  },
-  {
-    id: 3,
-    taskTitle: "Build a to-do list with React and TypeScript",
-    isComplete: false,
-  },
-  {
-    id: 4,
-    taskTitle: "Study how to create a SPA with React",
-    isComplete: false,
-  }
-];
-
-export const [tasksList, setTasksList] = useState<Tasks[]>(tasks)
-
-const [newTaskTitle, setNewTaskTitle] = useState<string>('')
-
-export function NewTask() {
-
-  function handleCreateNewTask(event: FormEvent) {
-    event.preventDefault();
-
-    const newTask = {
-      id: 5,
-      taskTitle: newTaskTitle,
-      isComplete: false,
-    }
-
-    setTasksList([...tasksList, newTask])
-    setNewTaskTitle('');
-  }
-
-  function handleNewTaskChange(event: ChangeEvent<HTMLTextAreaElement>) {
-    setNewTaskTitle(event.target.value)
-  }
+export function NewTask({ onCreateNewTask, newTaskTitle, onNewTaskChange }: NewTaskProps) {
+  const isNewTaskEmpty = newTaskTitle.length === 0;
 
   return (
     <section>
-      <form onSubmit={handleCreateNewTask} className={styles.newTask}>
+      <form onSubmit={onCreateNewTask} className={styles.newTask}>
         {/* Maybe add auto height adjust later */}
         <textarea 
           placeholder='Add a new task'
-          onChange={handleNewTaskChange}
+          value={newTaskTitle}
+          onChange={onNewTaskChange}
+          required
         />
 
-        <button type="submit">
+        <button type="submit" disabled={isNewTaskEmpty}>
           Add
           <PlusCircle size={30} weight='fill' />
         </button>      
